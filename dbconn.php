da55009ccada1185dd179e87a691fd809c14ca1f
MaartenVanNimwegen <89460431+MaartenVanNimwegen@users.noreply.github.com>
2023-02-20 14:51:33 +0100

    changed dbconn

--- dbconn.php
+++ dbconn.php
@@ -5,7 +5,7 @@ $username="root";
 $password="";
 $db="scrumapp";
 
-$conn= mysqli_connect($servername, $username, $password, $db);
+$conn = mysqli_connect($servername, $username, $password, $db);
 
 if(!$conn){
     echo "Connection Failed";
