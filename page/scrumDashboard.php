0e150c26c3e5b3a1995474cc88545ac28c12197d
Timhamer <240159@edu.rocfriesepoort.nl>
2023-02-21 08:51:08 +0100

    Folder fix

--- page/scrumDashboard.php
+++ page/scrumDashboard.php
@@ -1,3 +1,21 @@
+<?php
+include '../functions.php';
+include('dbconn.php');
+?>
+<script>
+    function myFunction() {
+        var x;
+        var r = confirm("Press OK or Cancel button");
+        if (r == true) {
+         x = "You pressed OK!";
+     }
+        else {
+         x = "You pressed Cancel!";
+     }
+     document.getElementById("demo").innerHTML = x;
+}
+</script>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +27,13 @@
 </head>
 <body>
     <div class="ScrumDashboardLayout">
-        
+    <button onclick="AddScrumgroepPopup()">Scrumgroep toevoegen</button>
+        <?php
+        if (isset($productID)) {
+            DeleteScrumgroep($conn);
+        }
+            ScrumgroepenTonen($conn)
+        ?>
     </div>
 </body>
 </html>
