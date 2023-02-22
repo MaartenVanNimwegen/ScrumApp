5a8850e7b37a799c0377d56e887154ccfaf54690
MaartenVanNimwegen <89460431+MaartenVanNimwegen@users.noreply.github.com>
2023-02-22 17:54:42 +0100

    Begin aan invulscherm formulier

--- page/reviewretroinvulscherm.php
+++ page/reviewretroinvulscherm.php
@@ -11,6 +11,46 @@
     <title>Review Retro invullen</title>
 </head>
 <body>
-    
+    <div class="invulscherm h-100 w-100 d-flex align-items-center justify-content-center">
+        <form action="" method="post">
+            <div class="retro w-100">
+                <h3>Retrospective</h3>
+
+                <label for="bijdrage">Wat was mijn bijdrage de afgelopen week?</label><br>
+                <textarea class="w-100" name="bijdrage" id="bijdrage" cols="50" rows="3"></textarea><br>
+
+                <label for="meerwaarden">Wat was jou meerwaarden?</label><br>
+                <textarea class="w-100" name="meerwaarden" id="meerwaarden" cols="50" rows="3"></textarea><br>
+                
+                <label for="tegenaan">Waar liep je tegenaan?</label><br>
+                <textarea class="w-100" name="tegenaan" id="tegenaan" rows="3"></textarea><br>
+
+                <label for="tips">Tip voor <?$naam?></label><br>
+                <input type="text">
+                <!-- <?php foreach($users as $naam){echo"";}?> -->
+
+                <label for="tops">Tops</label><br>
+                <textarea name="tops" id="tops" cols="50" rows="3"></textarea><br>
+            </div>
+            
+            <br>
+
+            <div class="review w-100">
+                <h3>Review</h3>
+
+                <label for="backlogitems">Aan welke backlogitems hebben jullie gewerkt?</label><br>
+                <textarea class="w-100" name="backlogitems" id="backlogitems" cols="50" rows="3"></textarea><br>
+
+                <label for="demonstreren">Wat gaan jullie demonstreren?</label><br>
+                <textarea class="w-100" name="demonstreren" id="demonstreren" cols="50" rows="3"></textarea><br>
+                
+                <label for="samenwerking">Hoe was jullie samenwerking?</label><br>
+                <textarea class="w-100" name="samenwerking" id="samenwerking" cols="50" rows="3"></textarea><br>
+
+                <label for="todoitems">Welke items gaan jullie de komende sprint werken?</label><br>
+                <textarea class="w-100" name="todoitems" id="todoitems" cols="50" rows="3"></textarea><br>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
