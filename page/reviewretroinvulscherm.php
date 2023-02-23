89d42e6420f40852cb9f4ed4f7c92c5b59df9247
Remon Dollenkamp <remon2004@outlook.com>
2023-02-23 13:18:50 +0100

    Login submit style fix.

--- page/reviewretroinvulscherm.php
+++ page/reviewretroinvulscherm.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../Styles/Style.css">
+    <title>Review invul</title>
 </head>
 <body>
     
