81ba908de93757770baa96b4acc9771da05fc9d2
Maarten van Nimwegen <maartenvannimwegen@hotmail.com>
2023-03-08 13:46:39 +0100

    added index vulling

--- index.php
+++ index.php
@@ -14,10 +14,11 @@
         if(empty($_SESSION['id'])) {
             header("Location: login.php");
         }
-        echo 'session: ';
-        print_r($_SESSION);
-
         ?>
+        <marquee scrollamount="30" direction="down" width="100%" height="900px" behavior="alternate">
+            <marquee scrollamount="30" style="font-size: 100px; position: absolute; z-index: 2; color: blue;" behavior="alternate">Welkom <?php echo $_SESSION['naam'];?></marquee>
+            <marquee scrollamount="30" style="font-size: 100px;" behavior="alternate"><img src="Image/6f3aa366-4eb6-37bd-b6ed-806b557a2c0e.jpg"></img></marquee>
+        </marquee>
     </div>
 </body>
 </html>
